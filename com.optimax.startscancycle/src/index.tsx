711a2e08ed91ce4bf6f3079ed404182243e0bd46
samicrb <130386561+samicrb@users.noreply.github.com>
2025-07-10 15:08:19 +0200

    last

--- com.optimax.startscancycle/src/index.tsx
+++ com.optimax.startscancycle/src/index.tsx
@@ -3,24 +3,56 @@
     Copyright (c) 2023, Doosan Robotics Inc.
 */
 //Dart-api
-import { System, BaseModule, ModuleContext, ModuleScreen, ModuleScreenProps, ModuleService} from 'dart-api';
+import { System, BaseModule, ModuleContext, ModuleScreen, ModuleScreenProps, ModuleService, logger} from 'dart-api';
 import DrlUtils from './DrlUtils';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Typography, ThemeProvider } from '@mui/material';
+// Screens
+import MainScreenReact       from './MainScreen';
+import StartScanCycleScreen from './StartScanCycleScreen';
+import SetValuesScreen from './SetValuesScreen';
+// Services
+import StartScanCycleService  from './StartScanCycleService';
+import SetValuesService      from './SetValuesService';
+
 
 // IIFE for register a function to create an instance of main class which is inherited BaseModule.
 (() => {
     System.registerModuleMainClassCreator((packageInfo) => new Module(packageInfo));
 })();
+
 class Module extends BaseModule {
     getModuleScreen(componentId: string) {
         if (componentId === 'MainScreen') {
             return MainScreen;
         }
+        
+        //for pip screen that opened when usr clicked the property tab in task editor module of dart-platform.
+        if (componentId === 'StartScanCycleScreen') {
+            return StartScanCycleScreen;
+        }
+        if (componentId === 'SetValuesScreen') {
+            return SetValuesScreen;
+        }
         return null;
     }
+    /* 2. Quel service fournir ? */
+    getModuleService(componentId: string): typeof ModuleService | null {
+        logger.debug(`getModuleService: ${this.packageInfo.packageName}, ${componentId}`);
+
+        switch (componentId) {
+        case 'start_scan_cycle':   
+            return StartScanCycleService;      
+        case 'set_values':
+            return SetValuesService;
+        default:
+            return null;
+        }
+    }
 }
+
+
 class MainScreen extends ModuleScreen {
     constructor(props: ModuleScreenProps) {
         super(props);
